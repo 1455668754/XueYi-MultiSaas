d72d30493658c1bf8a4a4ee66604dfb06198830a
雪忆天堂 <1455668754@qq.com>
2022-01-09 10:29:24 +0800

    定时任务目标字符串过滤特殊字符

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/Constants.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/Constants.java
@@ -45,7 +45,7 @@ public class Constants {
 
     /** 定时任务违规的字符 */
     public static final String[] JOB_ERROR_STR = {"java.net.URL", "javax.naming.InitialContext", "org.yaml.snakeyaml",
-            "org.springframework.jndi"};
+            "org.springframework"};
 
 
 }
