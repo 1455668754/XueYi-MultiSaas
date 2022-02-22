cce2a93bab8fac968d4dad7b3cf4c7ff5e92c9ee
奇点 <1455668754@qq.com>
2022-02-22 17:50:10 +0800

    1.部门管理接口下发

--- xueyi-modules/xueyi-system/src/main/java/com/xueyi/system/organize/controller/SysDeptController.java
+++ xueyi-modules/xueyi-system/src/main/java/com/xueyi/system/organize/controller/SysDeptController.java
@@ -4,16 +4,23 @@ import cn.hutool.core.util.StrUtil;
 import com.xueyi.common.core.constant.BaseConstants;
 import com.xueyi.common.core.domain.R;
 import com.xueyi.common.core.exception.ServiceException;
+import com.xueyi.common.core.web.result.AjaxResult;
+import com.xueyi.common.log.annotation.Log;
+import com.xueyi.common.log.enums.BusinessType;
 import com.xueyi.common.security.annotation.InnerAuth;
+import com.xueyi.common.security.annotation.Logical;
+import com.xueyi.common.security.annotation.RequiresPermissions;
 import com.xueyi.common.web.entity.controller.SubTreeController;
 import com.xueyi.system.api.organize.domain.dto.SysDeptDto;
 import com.xueyi.system.api.organize.domain.dto.SysPostDto;
 import com.xueyi.system.organize.service.ISysDeptService;
 import com.xueyi.system.organize.service.ISysPostService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * 部门管理 业务处理
@@ -25,11 +32,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SysDeptController extends SubTreeController<SysDeptDto, ISysDeptService, SysPostDto, ISysPostService> {
 
     /** 定义节点名称 */
+    @Override
     protected String getNodeName() {
         return "部门";
     }
 
     /** 定义子数据名称 */
+    @Override
     protected String getSubName() {
         return "岗位";
     }
@@ -41,6 +50,98 @@ public class SysDeptController extends SubTreeController<SysDeptDto, ISysDeptSer
     @PostMapping("/inner/add")
     public R<SysDeptDto> addInner(@RequestBody SysDeptDto dept) {
         return baseService.addInner(dept) > 0 ? R.ok(dept) : R.fail();
+    }
+
+    /**
+     * 查询部门列表
+     */
+    @Override
+    @GetMapping("/list")
+    @RequiresPermissions("organize:dept:list")
+    public AjaxResult listExtra(SysDeptDto dept) {
+        return super.listExtra(dept);
+    }
+
+    /**
+     * 查询部门列表（排除节点）
+     */
+    @GetMapping("/list/exclude")
+    @RequiresPermissions("organize:dept:list")
+    public AjaxResult listExNodes(SysDeptDto dept) {
+        return super.listExNodes(dept);
+    }
+
+    /**
+     * 查询部门详细
+     */
+    @Override
+    @GetMapping(value = "/{id}")
+    @RequiresPermissions("organize:dept:single")
+    public AjaxResult getInfoExtra(@PathVariable Serializable id) {
+        return super.getInfoExtra(id);
+    }
+
+    /**
+     * 部门导出
+     */
+    @Override
+    @PostMapping("/export")
+    @RequiresPermissions("organize:dept:export")
+    public void export(HttpServletResponse response, SysDeptDto dept) {
+        super.export(response, dept);
+    }
+
+    /**
+     * 部门新增
+     */
+    @Override
+    @PostMapping
+    @RequiresPermissions("organize:dept:add")
+    @Log(title = "部门管理", businessType = BusinessType.INSERT)
+    public AjaxResult add(@Validated @RequestBody SysDeptDto dept) {
+        return super.add(dept);
+    }
+
+    /**
+     * 部门修改
+     */
+    @Override
+    @PutMapping
+    @RequiresPermissions("organize:dept:edit")
+    @Log(title = "部门管理", businessType = BusinessType.UPDATE)
+    public AjaxResult edit(@Validated @RequestBody SysDeptDto dept) {
+        return super.edit(dept);
+    }
+
+    /**
+     * 部门修改状态
+     */
+    @Override
+    @PutMapping("/status")
+    @RequiresPermissions(value = {"organize:dept:edit", "organize:dept:editStatus"}, logical = Logical.OR)
+    @Log(title = "部门管理", businessType = BusinessType.UPDATE_STATUS)
+    public AjaxResult editStatus(@RequestBody SysDeptDto dept) {
+        return super.editStatus(dept);
+    }
+
+    /**
+     * 部门批量删除
+     */
+    @Override
+    @DeleteMapping("/batch/{idList}")
+    @RequiresPermissions("organize:dept:delete")
+    @Log(title = "部门管理", businessType = BusinessType.DELETE)
+    public AjaxResult batchRemove(@PathVariable List<Long> idList) {
+        return super.batchRemove(idList);
+    }
+
+    /**
+     * 获取部门选择框列表
+     */
+    @Override
+    @GetMapping("/option")
+    public AjaxResult option() {
+        return super.option();
     }
 
 //    /**
