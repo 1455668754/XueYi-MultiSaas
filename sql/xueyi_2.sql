280e482d37879ad849bea9c5b20eb787d7b5aa11
奇点 <1455668754@qq.com>
2022-03-11 12:56:02 +0800

    v2.1.3版本

--- sql/xueyi_2.sql
+++ sql/xueyi_2.sql
@@ -1,3 +1,12 @@
+# DROP DATABASE IF EXISTS `xy-cloud1`;
+#
+# CREATE DATABASE  `xy-cloud1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+# SET NAMES utf8mb4;
+# SET FOREIGN_KEY_CHECKS = 0;
+#
+# USE `xy-cloud1`;
+
 SET NAMES utf8mb4;
 
 -- ----------------------------
