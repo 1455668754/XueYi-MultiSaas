c97dd3d487ad2329287d402d7bb2e3c8e06d2b16
雪忆 <1455668754@qq.com>
2022-08-01 09:25:53 +0800

    fix: 用户头像上传限制只能为图片格式

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/file/FileTypeUtils.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/file/FileTypeUtils.java
@@ -1,8 +1,11 @@
 package com.xueyi.common.core.utils.file;
 
-import java.io.File;
-
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
 
 /**
  * 文件类型工具类
@@ -43,6 +46,20 @@ public class FileTypeUtils {
     }
 
     /**
+     * 获取文件名的后缀
+     *
+     * @param file 表单文件
+     * @return 后缀名
+     */
+    public static String getExtension(MultipartFile file) {
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        if (StringUtils.isEmpty(extension)) {
+            extension = MimeTypeUtils.getExtension(Objects.requireNonNull(file.getContentType()));
+        }
+        return extension;
+    }
+
+    /**
      * 获取文件类型
      *
      * @param photoByte 文件字节码
@@ -50,8 +67,7 @@ public class FileTypeUtils {
      */
     public static String getFileExtendName(byte[] photoByte) {
         String strFileExtendName = "JPG";
-        if ((photoByte[0] == 71) && (photoByte[1] == 73) && (photoByte[2] == 70) && (photoByte[3] == 56)
-                && ((photoByte[4] == 55) || (photoByte[4] == 57)) && (photoByte[5] == 97)) {
+        if ((photoByte[0] == 71) && (photoByte[1] == 73) && (photoByte[2] == 70) && (photoByte[3] == 56) && ((photoByte[4] == 55) || (photoByte[4] == 57)) && (photoByte[5] == 97)) {
             strFileExtendName = "GIF";
         } else if ((photoByte[6] == 74) && (photoByte[7] == 70) && (photoByte[8] == 73) && (photoByte[9] == 70)) {
             strFileExtendName = "JPG";
