008e4c6f4ae2dffadc200a698455a2031ad9def2
雪忆 <1455668754@qq.com>
2022-10-10 12:20:20 +0800

    fix: 兼容rowKey有可能为number的情况
    (cherry picked from commit f5f76ad7721b05e903bf9e30ee33e86a1f5e364d)

--- multi-ui/src/components/Table/src/types/table.ts
+++ multi-ui/src/components/Table/src/types/table.ts
@@ -86,7 +86,7 @@ export interface TableActionType {
   getSelectRows: <T = Recordable>() => T[];
   clearSelectedRowKeys: () => void;
   expandAll: () => void;
-  expandRows: (keys: string[]) => void;
+  expandRows: (keys: string[] | number[]) => void;
   collapseAll: () => void;
   scrollTo: (pos: string) => void; // pos: id | "top" | "bottom"
   getSelectRowKeys: () => string[];
