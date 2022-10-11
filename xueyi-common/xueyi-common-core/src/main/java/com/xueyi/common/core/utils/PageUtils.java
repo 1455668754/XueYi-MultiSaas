6c6daecc5e185b87c559dad2f6805485069ab594
雪忆 <1455668754@qq.com>
2022-10-11 11:14:36 +0800

    fix: 移除分页参数默认值引致列表数据查询分页
    (cherry picked from commit 44f48be903109e133ea59b8d552623242c2771d2)

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/PageUtils.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/PageUtils.java
@@ -1,10 +1,10 @@
 package com.xueyi.common.core.utils;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.xueyi.common.core.utils.sql.SqlUtil;
 import com.xueyi.common.core.web.page.PageDomain;
 import com.xueyi.common.core.web.page.TableSupport;
-import org.apache.commons.lang3.ObjectUtils;
 
 /**
  * 分页工具类
@@ -20,7 +20,7 @@ public class PageUtils extends PageHelper {
         PageDomain pageDomain = TableSupport.buildPageRequest();
         Integer pageNum = pageDomain.getPageNum();
         Integer pageSize = pageDomain.getPageSize();
-        if (ObjectUtils.allNotNull(pageNum, pageSize)) {
+        if (ObjectUtil.isAllNotEmpty(pageNum, pageSize)) {
             String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
             Boolean reasonable = pageDomain.getReasonable();
             PageHelper.startPage(pageNum, pageSize, orderBy).setReasonable(reasonable);
