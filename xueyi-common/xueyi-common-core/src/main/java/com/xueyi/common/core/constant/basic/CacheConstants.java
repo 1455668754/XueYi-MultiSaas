df2e21eda638e195b0e3fc10e307d656c0150421
雪忆天堂 <1455668754@qq.com>
2022-12-02 15:23:59 +0800

    fix: 移除无用缓存字段

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/basic/CacheConstants.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/basic/CacheConstants.java
@@ -22,9 +22,6 @@ public class CacheConstants {
     /** 验证码 redis key */
     public static final String CAPTCHA_CODE_KEY = "captcha_codes:";
 
-    /** 源策略组管理 cache key */
-    public static final String DATA_SOURCE_KEY = "system:source";
-
     /** 登录缓存类型 */
     @Getter
     @AllArgsConstructor
