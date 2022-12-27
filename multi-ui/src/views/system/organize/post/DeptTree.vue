36f6f5fa66e3fb0e9985eca75c032d7bddbeaba6
雪忆天堂 <1455668754@qq.com>
2022-12-27 10:47:12 +0800

    fix: 修正岗位管理、用户管理组织查询无法滚动问题

--- multi-ui/src/views/system/organize/post/DeptTree.vue
+++ multi-ui/src/views/system/organize/post/DeptTree.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="m-4 mr-0 overflow-hidden bg-white">
-    <BasicTree
-      title="部门列表"
-      toolbar
-      search
-      :clickRowToExpand="false"
-      :treeData="treeData"
-      :fieldNames="{ key: 'id', title: 'name' }"
-      @select="handleSelect"
-    />
+  <div class="m-4 mr-0 overflow-hidden bg-white scroll-wrap">
+    <ScrollContainer>
+      <BasicTree
+        title="部门列表"
+        toolbar
+        search
+        :clickRowToExpand="false"
+        :treeData="treeData"
+        :fieldNames="{ key: 'id', title: 'name' }"
+        @select="handleSelect"
+      />
+    </ScrollContainer>
   </div>
 </template>
 
@@ -16,11 +18,11 @@
   import { defineComponent, onMounted, ref } from 'vue';
   import { BasicTree, TreeItem } from '/@/components/Tree';
   import { optionDeptApi } from '/@/api/system/organize/dept';
+  import { ScrollContainer } from '/@/components/Container';
 
   export default defineComponent({
     name: 'DeptTree',
-    components: { BasicTree },
-
+    components: { ScrollContainer, BasicTree },
     emits: ['select'],
     setup(_, { emit }) {
       const treeData = ref<TreeItem[]>([]);
