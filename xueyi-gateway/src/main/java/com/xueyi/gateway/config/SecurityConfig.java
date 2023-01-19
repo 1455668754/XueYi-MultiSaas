6c172045deb2405ac49a63affb28fab573b5f838
雪忆天堂 <1455668754@qq.com>
2023-01-19 18:34:39 +0800

    feat: 调整SpringSecurity鉴权

--- xueyi-gateway/src/main/java/com/xueyi/gateway/config/SecurityConfig.java
+++ xueyi-gateway/src/main/java/com/xueyi/gateway/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
     @Autowired
     private IgnoreWhiteProperties ignoreWhite;
