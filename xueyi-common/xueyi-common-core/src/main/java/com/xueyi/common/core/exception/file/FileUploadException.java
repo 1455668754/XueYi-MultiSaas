2a805e921bb5eb10321e8844e84e18d880627d87
雪忆天堂 <1455668754@qq.com>
2023-02-27 12:45:50 +0800

    fix: 移除apache/commons-fileupload依赖
    (cherry picked from commit f086af3ca67566459750521dc1b363fe44bbcd9a)

---
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/exception/file/FileUploadException.java
@@ -0,0 +1,55 @@
+package com.xueyi.common.core.exception.file;
+
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.Serial;
+
+/**
+ * 文件上传异常类
+ *
+ * @author ruoyi
+ */
+public class FileUploadException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final Throwable cause;
+
+    public FileUploadException() {
+        this(null, null);
+    }
+
+    public FileUploadException(final String msg) {
+        this(msg, null);
+    }
+
+    public FileUploadException(String msg, Throwable cause) {
+        super(msg);
+        this.cause = cause;
+    }
+
+    @Override
+    public void printStackTrace(PrintStream stream) {
+        super.printStackTrace(stream);
+        if (cause != null) {
+            stream.println("Caused by:");
+            cause.printStackTrace(stream);
+        }
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter writer) {
+        super.printStackTrace(writer);
+        if (cause != null) {
+            writer.println("Caused by:");
+            cause.printStackTrace(writer);
+        }
+    }
+
+    @Override
+    public Throwable getCause() {
+        return cause;
+    }
+}
--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/exception/file/InvalidExtensionException.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/exception/file/InvalidExtensionException.java
@@ -1,17 +1,14 @@
 package com.xueyi.common.core.exception.file;
 
-import org.apache.commons.fileupload.FileUploadException;
-
 import java.io.Serial;
 import java.util.Arrays;
 
 /**
  * 文件上传 误异常类
- * 
+ *
  * @author xueyi
  */
-public class InvalidExtensionException extends FileUploadException
-{
+public class InvalidExtensionException extends FileUploadException {
     @Serial
     private static final long serialVersionUID = 1L;
 
@@ -19,69 +16,57 @@ public class InvalidExtensionException extends FileUploadException
     private String extension;
     private String filename;
 
-    public InvalidExtensionException(String[] allowedExtension, String extension, String filename)
-    {
+    public InvalidExtensionException(String[] allowedExtension, String extension, String filename) {
         super("filename : [" + filename + "], extension : [" + extension + "], allowed extension : [" + Arrays.toString(allowedExtension) + "]");
         this.allowedExtension = allowedExtension;
         this.extension = extension;
         this.filename = filename;
     }
 
-    public String[] getAllowedExtension()
-    {
+    public String[] getAllowedExtension() {
         return allowedExtension;
     }
 
-    public String getExtension()
-    {
+    public String getExtension() {
         return extension;
     }
 
-    public String getFilename()
-    {
+    public String getFilename() {
         return filename;
     }
 
-    public static class InvalidImageExtensionException extends InvalidExtensionException
-    {
+    public static class InvalidImageExtensionException extends InvalidExtensionException {
         @Serial
-    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-        public InvalidImageExtensionException(String[] allowedExtension, String extension, String filename)
-        {
+        public InvalidImageExtensionException(String[] allowedExtension, String extension, String filename) {
             super(allowedExtension, extension, filename);
         }
     }
 
-    public static class InvalidFlashExtensionException extends InvalidExtensionException
-    {
+    public static class InvalidFlashExtensionException extends InvalidExtensionException {
         @Serial
-    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-        public InvalidFlashExtensionException(String[] allowedExtension, String extension, String filename)
-        {
+        public InvalidFlashExtensionException(String[] allowedExtension, String extension, String filename) {
             super(allowedExtension, extension, filename);
         }
     }
 
-    public static class InvalidMediaExtensionException extends InvalidExtensionException
-    {
+    public static class InvalidMediaExtensionException extends InvalidExtensionException {
         @Serial
-    private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-        public InvalidMediaExtensionException(String[] allowedExtension, String extension, String filename)
-        {
+        public InvalidMediaExtensionException(String[] allowedExtension, String extension, String filename) {
             super(allowedExtension, extension, filename);
         }
     }
-    
-    public static class InvalidVideoExtensionException extends InvalidExtensionException
-    {
-        @Serial
-    private static final long serialVersionUID = 1L;
 
-        public InvalidVideoExtensionException(String[] allowedExtension, String extension, String filename)
-        {
+    public static class InvalidVideoExtensionException extends InvalidExtensionException {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        public InvalidVideoExtensionException(String[] allowedExtension, String extension, String filename) {
             super(allowedExtension, extension, filename);
         }
     }
