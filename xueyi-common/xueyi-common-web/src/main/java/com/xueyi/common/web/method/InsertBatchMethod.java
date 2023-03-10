000ddc9cab6d8053f92d4b69e1de3607635e655a
雪忆天堂 <1455668754@qq.com>
2023-03-10 12:15:57 +0800

    fix: 修正批量新增注入值异常问题
    (cherry picked from commit 363d236ab71a2aa7a30b8a07a4ae571926c17456)

--- xueyi-common/xueyi-common-web/src/main/java/com/xueyi/common/web/method/InsertBatchMethod.java
+++ xueyi-common/xueyi-common-web/src/main/java/com/xueyi/common/web/method/InsertBatchMethod.java
@@ -79,12 +79,11 @@ public class InsertBatchMethod extends AbstractMethod {
         final StringBuilder valueSql = new StringBuilder();
         valueSql.append("<foreach collection=\"collection\" item=\"item\" index=\"index\" open=\"(\" separator=\"),(\" close=\")\">");
         if (StrUtil.isNotEmpty(tableInfo.getKeyColumn()))
-            valueSql.append("#{item.").append(tableInfo.getKeyProperty()).append("},");
-        tableInfo.getFieldList().forEach(x -> {
-            if (!ObjectUtil.equals(FieldStrategy.NEVER, x.getInsertStrategy()))
-                valueSql.append("#{item.").append(x.getProperty()).append("},");
-        });
-        valueSql.delete(valueSql.length() - 1, valueSql.length());
+            valueSql.append("#{item.").append(tableInfo.getKeyProperty()).append(StrUtil.DELIM_END).append(StrUtil.COMMA);
+        tableInfo.getFieldList().stream()
+                .filter(x -> !(ObjectUtil.equals(FieldStrategy.NEVER, x.getInsertStrategy()) || x.isLogicDelete()))
+                .forEach(x -> valueSql.append("#{item.").append(x.getProperty()).append(StrUtil.DELIM_END).append(StrUtil.COMMA));
+        valueSql.delete(valueSql.length() - NumberUtil.One, valueSql.length());
         valueSql.append("</foreach>");
         return valueSql.toString();
     }
