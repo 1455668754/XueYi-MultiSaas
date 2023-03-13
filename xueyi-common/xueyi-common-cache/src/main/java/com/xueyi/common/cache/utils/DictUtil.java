23a92100780a320ad18afa65e1ca676f8c0d32d3
雪忆天堂 <1455668754@qq.com>
2023-03-13 16:05:29 +0800

    fix: 优化参数缓存管理工具
    (cherry picked from commit 1ead44816db2f2f3c4bbd305ce635ff3611232a1)

--- xueyi-common/xueyi-common-cache/src/main/java/com/xueyi/common/cache/utils/DictUtil.java
+++ xueyi-common/xueyi-common-cache/src/main/java/com/xueyi/common/cache/utils/DictUtil.java
@@ -2,7 +2,9 @@ package com.xueyi.common.cache.utils;
 
 import com.xueyi.common.cache.service.CacheService;
 import com.xueyi.common.core.constant.basic.CacheConstants;
+import com.xueyi.common.core.utils.core.ConvertUtil;
 import com.xueyi.common.core.utils.core.SpringUtil;
+import com.xueyi.common.core.utils.core.StrUtil;
 import com.xueyi.system.api.dict.domain.dto.SysDictDataDto;
 
 import java.util.Collection;
@@ -16,13 +18,34 @@ import java.util.List;
 public class DictUtil {
 
     /**
-     * 获取字典缓存
+     * 获取参数缓存 | 字符串
      *
-     * @param code 字典编码
-     * @return 字典数据列表
+     * @param code 参数编码
+     * @return 参数数据
      */
-    public static List<SysDictDataDto> getConfigCache(String code) {
-        return SpringUtil.getBean(CacheService.class).getCacheObject(CacheConstants.CacheType.SYS_CONFIG_KEY, code);
+    public static String getConfigCacheToStr(String code) {
+        return getConfigCache(code, String.class);
+    }
+
+    /**
+     * 获取参数缓存 | 数值
+     *
+     * @param code 参数编码
+     * @return 参数数据
+     */
+    public static Integer getConfigCacheToInt(String code) {
+        return getConfigCache(code, Integer.class);
+    }
+
+    /**
+     * 获取参数缓存
+     *
+     * @param code 参数编码
+     * @return 参数数据
+     */
+    public static <T> T getConfigCache(String code, Class<T> clazz) {
+        String value = SpringUtil.getBean(CacheService.class).getCacheObject(CacheConstants.CacheType.SYS_CONFIG_KEY, code);
+        return StrUtil.isNotEmpty(value) ? ConvertUtil.convert(clazz, value) : null;
     }
 
     /**
