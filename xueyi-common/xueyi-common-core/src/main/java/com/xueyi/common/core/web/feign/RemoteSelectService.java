8b065d2f9686b815ea0cda7daa4dd4adca3d3870
雪忆天堂 <1455668754@qq.com>
2023-05-22 12:04:35 +0800

    fix: 增加远程查询服务层

---
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/web/feign/RemoteSelectService.java
@@ -0,0 +1,37 @@
+package com.xueyi.common.core.web.feign;
+
+import com.xueyi.common.core.constant.basic.SecurityConstants;
+import com.xueyi.common.core.web.entity.base.BaseEntity;
+import com.xueyi.common.core.web.result.R;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 远程查询服务层
+ *
+ * @param <D> Dto
+ * @author xueyi
+ */
+public interface RemoteSelectService<D extends BaseEntity> {
+
+    /**
+     * 根据Id查询数据信息
+     *
+     * @param id Id
+     * @return 数据信息
+     */
+    @PostMapping(value = "/id", headers = SecurityConstants.FROM_SOURCE_INNER)
+    R<D> selectByIdInner(Serializable id);
+
+    /**
+     * 根据Ids查询数据信息集合
+     *
+     * @param ids Ids
+     * @return 数据信息集合
+     */
+    @PostMapping(value = "/ids", headers = SecurityConstants.FROM_SOURCE_INNER)
+    R<List<D>> selectListByIdsInner(Collection<? extends Serializable> ids);
+}
