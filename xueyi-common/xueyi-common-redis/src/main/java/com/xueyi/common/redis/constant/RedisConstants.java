c208efbe35122c063362a5513f2cbe3858c6f9fa
雪忆天堂 <1455668754@qq.com>
2023-06-10 14:00:38 +0800

    fix: 增加数据关联工具缓存键

--- xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/constant/RedisConstants.java
+++ xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/constant/RedisConstants.java
@@ -20,6 +20,7 @@ public class RedisConstants {
         CLIENT_DETAILS_KEY("client:details", "oauth 客户端信息"),
         CAPTCHA_CODE_KEY("captcha_codes:", "验证码"),
         SYS_CORRELATE_KEY("system:correlate:{}.{}", "数据关联工具"),
+        SYS_CORRELATE_IMPL_KEY("system:correlate:service:{}", "数据关联工具"),
         SERVICE_WX_MP_KEY("service:wxMp", "服务：微信公众号"),
         SERVICE_WX_MA_KEY("service:wxMa", "服务：微信小程序"),
         SERVICE_WX_PAY_KEY("service:wxPay", "服务：微信支付");
