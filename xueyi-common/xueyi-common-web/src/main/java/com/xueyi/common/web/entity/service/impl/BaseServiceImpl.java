eb61d41403a1c63563bddf9575d2d4c91c83416d
雪忆天堂 <1455668754@qq.com>
2023-06-13 19:40:44 +0800

    fix: 修正缓存更新异常

--- xueyi-common/xueyi-common-web/src/main/java/com/xueyi/common/web/entity/service/impl/BaseServiceImpl.java
+++ xueyi-common/xueyi-common-web/src/main/java/com/xueyi/common/web/entity/service/impl/BaseServiceImpl.java
@@ -271,6 +271,6 @@ public class BaseServiceImpl<Q extends BaseEntity, D extends BaseEntity, C exten
             throw new UtilException("未正常配置缓存，无法使用!");
         }
         List<D> allList = selectListMerge(null);
-        refreshCache(null, RedisConstants.OperateType.REFRESH_ALL, null, allList);
+        refreshCache(OperateConstants.ServiceType.BATCH_ADD, RedisConstants.OperateType.REFRESH_ALL, null, allList);
     }
 }
