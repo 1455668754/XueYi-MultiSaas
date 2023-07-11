325ea57673c769e4a3f3604445cf7b6e60b13022
雪忆天堂 <1455668754@qq.com>
2023-07-11 09:42:24 +0800

    fix: 修正swagger鉴权
    (cherry picked from commit 1d54c31104afe8da4587df49713bd2211d1da3bd)

--- xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerAutoConfiguration.java
+++ xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerAutoConfiguration.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -52,11 +54,24 @@ public class SwaggerAutoConfiguration {
                 .email(swaggerProperties.getContact().getEmail());
     }
 
+
     /**
      * 默认的全局鉴权策略
      */
     private Components components() {
         return new Components()
-                .addSecuritySchemes("Authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("Authorization").bearerFormat("JWT"));
+                .addSecuritySchemes("token",
+                        new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name("Authorization").bearerFormat("JWT"))
+                ;
+    }
+
+    @Bean
+    public GroupedOpenApi usersGroup() {
+        return GroupedOpenApi.builder().group("all")
+                .addOperationCustomizer((operation, handlerMethod) -> {
+                    operation.addSecurityItem(new SecurityRequirement().addList("token"));
+                    return operation;
+                })
+                .build();
     }
 }
