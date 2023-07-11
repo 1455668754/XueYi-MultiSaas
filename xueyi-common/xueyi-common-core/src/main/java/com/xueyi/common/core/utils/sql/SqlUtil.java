30995fdd840e23cd863ae283b7d3ea77363df87d
雪忆天堂 <1455668754@qq.com>
2023-07-11 16:07:10 +0800

    feat: 排序属性orderBy参数限制长度

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/sql/SqlUtil.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/sql/SqlUtil.java
@@ -16,12 +16,18 @@ public class SqlUtil {
     /** 仅支持字母、数字、下划线、空格、逗号、小数点（支持多个字段排序） */
     public static String SQL_PATTERN = "[a-zA-Z0-9_\\ \\,\\.]+";
 
+    /** 限制orderBy最大长度 */
+    private static final int ORDER_BY_MAX_LENGTH = 500;
+
     /**
      * 检查字符，防止注入绕过
      */
     public static String escapeOrderBySql(String value) {
         if (StrUtil.isNotEmpty(value) && !isValidOrderBySql(value)) {
             throw new UtilException("参数不符合规范，不能进行查询");
+        }
+        if (StrUtil.length(value) > ORDER_BY_MAX_LENGTH) {
+            throw new UtilException("参数已超过最大限制，不能进行查询");
         }
         return value;
     }
