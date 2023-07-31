ca65990f0792f54f2107d2db0418aa290174f3a6
雪忆天堂 <1455668754@qq.com>
2023-07-31 15:18:24 +0800

    fix: 重构Form图片上传组件

--- multi-ui/src/components/Form/src/components/ImageUpload.vue
+++ multi-ui/src/components/Form/src/components/ImageUpload.vue
@@ -2,13 +2,11 @@
   <div class="clearfix">
     <a-upload
       v-model:file-list="fileList"
-      v-model:value="state"
       :list-type="listType"
       :multiple="multiple"
       :max-count="maxCount"
       :customRequest="handleCustomRequest"
       :before-upload="handleBeforeUpload"
-      @change="handleChange"
       @preview="handlePreview"
     >
       <div v-if="fileList.length < maxCount">
@@ -24,178 +22,183 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
   import { PlusOutlined } from '@ant-design/icons-vue';
-  import { defineComponent, PropType, ref, watch } from 'vue';
+  import { PropType, reactive, ref, watch } from 'vue';
   import { message, Modal, Upload, UploadProps } from 'ant-design-vue';
   import { UploadFile } from 'ant-design-vue/lib/upload/interface';
-  import { useRuleFormItem } from '/@/hooks/component/useFormItem';
   import { useI18n } from '/@/hooks/web/useI18n';
-  import { buildShortUUID } from '/@/utils/uuid';
-  import { fileUploadApi } from '@/api/sys/upload.api';
-  import { isEmpty } from 'lodash-es';
+  import { fileUploadApi } from '/@/api/sys/upload.api';
+  import { join } from 'lodash-es';
+  import { buildShortUUID } from '@/utils/uuid';
+  import { isUrl } from '@/utils/is';
+  import { useRuleFormItem } from '@/hooks/component/useFormItem';
 
-  export type ImageUploadType = 'text' | 'picture' | 'picture-card';
-  export default defineComponent({
-    name: 'ImageUpload',
-    components: {
-      PlusOutlined,
-      AUpload: Upload,
-      AModal: Modal,
+  type ImageUploadType = 'text' | 'picture' | 'picture-card';
+
+  const emit = defineEmits(['update:value']);
+  const props = defineProps({
+    value: [Array, String],
+    api: {
+      type: Function as PropType<(file: UploadFile) => Promise<any>>,
+      default: fileUploadApi,
     },
-    props: {
-      value: [Array, String],
-      api: {
-        type: Function as PropType<(file: UploadFile) => Promise<any>>,
-        default: fileUploadApi,
-      },
-      listType: {
-        type: String as PropType<ImageUploadType>,
-        default: 'picture-card',
-      },
-      // 文件类型
-      fileType: {
-        type: Array,
-        default: () => ['image/png', 'image/jpeg'],
-      },
-      multiple: { type: Boolean, default: false },
-      // 最大数量的文件
-      maxCount: { type: Number, default: 1 },
-      // 最小数量的文件
-      minCount: { type: Number, default: 0 },
-      // 文件最大多少MB
-      maxSize: { type: Number, default: 2 },
+    listType: {
+      type: String as PropType<ImageUploadType>,
+      default: () => 'picture-card',
     },
-    emits: ['update:value'],
-    setup(props, {}) {
-      const { t } = useI18n();
-      const previewVisible = ref(false);
-      const previewImage = ref('');
-      const fileList = ref<UploadFile[]>([]);
-      const isFirstLoad = ref(true);
-
-      // Embedded in the form, just use the hook binding to perform form verification
-      const [state] = useRuleFormItem(props);
-
-      watch(
-        () => state.value,
-        (count) => {
-          if (isFirstLoad.value) {
-            fileList.value = [];
-            if (count !== undefined) {
-              if (count && Array.isArray(count)) {
-                for (let i = 0; i < count.length; i++) {
-                  const uuid = buildShortUUID();
-                  fileList.value.push({
-                    uid: uuid,
-                    name: uuid,
-                    status: 'done',
-                    url: count[i] as string,
-                  });
-                }
-              } else {
-                const uuid = buildShortUUID();
-                fileList.value.push({
-                  uid: uuid,
-                  name: uuid,
-                  status: 'done',
-                  url: count as string,
-                });
-              }
-            }
-          }
-        },
-      );
-
-      /** 关闭查看 */
-      const handleCancel = () => {
-        previewVisible.value = false;
-      };
-
-      /** 查看图片 */
-      // @ts-ignore
-      const handlePreview = async (file: UploadProps['fileList'][number]) => {
-        if (!file.url && !file.preview) {
-          file.preview = (await getBase64(file.originFileObj)) as string;
-        }
-        previewImage.value = file.url || file.preview;
-        previewVisible.value = true;
-      };
-
-      /** 上传前校验 */
-      const handleBeforeUpload: UploadProps['beforeUpload'] = (file) => {
-        if (fileList.value.length > props.maxCount) {
-          fileList.value.splice(props.maxCount, fileList.value.length - props.maxCount);
-          message.error(t('component.upload.maxNumber', [props.maxCount]));
-          return false;
-        }
-        const isPNG = props.fileType.includes(file.type);
-        if (!isPNG) {
-          message.error(t('component.upload.acceptUpload', [props.fileType.toString()]));
-        }
-        const isLt2M = file.size / 1024 / 1024 < props.maxSize;
-        if (!isLt2M) {
-          message.error(t('component.upload.maxSizeMultiple', [props.maxSize]));
-        }
-        if (!(isPNG && isLt2M)) {
-          fileList.value.pop();
-        }
-        return isPNG && isLt2M;
-      };
-
-      /** 自定义上传 */
-      const handleCustomRequest = async (option: any) => {
-        isFirstLoad.value = false;
-        const { file } = option;
-        await props
-          .api(option)
-          .then((res) => {
-            file.status = 'done';
-            file.url = res.data.url;
-            fileList.value.pop();
-            fileList.value.push(file);
-            handleChange();
-          })
-          .catch(() => {
-            fileList.value.pop();
-          });
-        isFirstLoad.value = true;
-      };
-
-      function handleChange() {
-        isFirstLoad.value = false;
-        const images = fileList.value
-          .map((item) => {
-            return item.url;
-          })
-          .filter((item) => {
-            return !isEmpty(item);
-          });
-        state.value =
-          props.maxCount === 1
-            ? images.length > 0
-              ? images[0]
-              : ''
-            : images.length > 0
-            ? images
-            : [];
-        isFirstLoad.value = true;
-      }
-
-      return {
-        t,
-        state,
-        previewVisible,
-        previewImage,
-        fileList,
-        handleCancel,
-        handleChange,
-        handlePreview,
-        handleCustomRequest,
-        handleBeforeUpload,
-      };
+    // 文件类型
+    fileType: {
+      type: Array,
+      default: () => ['image/png', 'image/jpeg'],
+    },
+    multiple: {
+      type: Boolean,
+      default: () => false,
+    },
+    // 最大数量的文件
+    maxCount: {
+      type: Number,
+      default: () => 1,
+    },
+    // 最小数量的文件
+    minCount: {
+      type: Number,
+      default: () => 0,
+    },
+    // 文件最大多少MB
+    maxSize: {
+      type: Number,
+      default: () => 2,
     },
   });
+
+  // Embedded in the form, just use the hook binding to perform form verification
+  const [state] = useRuleFormItem(props);
+
+  const AUpload = Upload;
+  const AModal = Modal;
+  const { t } = useI18n();
+  const previewVisible = ref(false);
+  const previewImage = ref('');
+  const fileList = ref<UploadFile[]>([]);
+
+  const fileState = reactive<{
+    newList: any[];
+    newStr: string;
+    oldStr: string;
+    isLoad: boolean;
+  }>({
+    newList: [],
+    newStr: '',
+    oldStr: '',
+    isLoad: false,
+  });
+
+  watch(
+    () => fileList.value,
+    (v) => {
+      fileState.newList = v
+        .filter((item) => {
+          return item?.url && item.status === 'done' && isUrl(item?.url);
+        })
+        .map((item) => item?.url);
+      fileState.newStr = join(fileState.newList);
+      // 不相等代表数据变更
+      if (fileState.newStr !== fileState.oldStr) {
+        fileState.oldStr = fileState.newStr;
+        if (props.multiple) {
+          state.value = fileState.newList;
+        } else {
+          state.value = fileState.newStr;
+        }
+      }
+    },
+    {
+      deep: true,
+    },
+  );
+
+  watch(
+    () => state.value,
+    (v) => {
+      if (!fileState.isLoad) {
+        const stateStr = props.multiple ? join((v as any[]) || []) : v || '';
+        if (stateStr !== fileState.oldStr) {
+          fileState.isLoad = true;
+          fileList.value = [];
+          const list = v !== undefined ? (props.multiple ? (v as string[]) : [v as string]) : [];
+          for (let i = 0; i < list.length; i++) {
+            const uuid = buildShortUUID();
+            fileList.value.push({
+              uid: uuid,
+              name: uuid,
+              status: 'done',
+              url: list[i],
+            });
+          }
+          fileState.isLoad = false;
+        } else {
+          emit('update:value', v);
+        }
+      }
+    },
+    {
+      immediate: true,
+    },
+  );
+
+  /** 关闭查看 */
+  const handleCancel = () => {
+    previewVisible.value = false;
+  };
+
+  /** 查看图片 */
+  // @ts-ignore
+  const handlePreview = async (file: UploadProps['fileList'][number]) => {
+    if (!file.url && !file.preview) {
+      file.preview = (await getBase64(file.originFileObj)) as string;
+    }
+    previewImage.value = file.url || file.preview;
+    previewVisible.value = true;
+  };
+
+  /** 上传前校验 */
+  const handleBeforeUpload: UploadProps['beforeUpload'] = (file) => {
+    if (fileList.value.length > props.maxCount) {
+      fileList.value.splice(props.maxCount, fileList.value.length - props.maxCount);
+      message.error(t('component.upload.maxNumber', [props.maxCount]));
+      return Upload.LIST_IGNORE;
+    }
+    const isPNG = props.fileType.includes(file.type);
+    if (!isPNG) {
+      message.error(t('component.upload.acceptUpload', [props.fileType.toString()]));
+    }
+    const isLt2M = file.size / 1024 / 1024 < props.maxSize;
+    if (!isLt2M) {
+      message.error(t('component.upload.maxSizeMultiple', [props.maxSize]));
+    }
+    if (!(isPNG && isLt2M)) {
+      fileList.value.pop();
+    }
+    return (isPNG && isLt2M) || Upload.LIST_IGNORE;
+  };
+
+  /** 自定义上传 */
+  const handleCustomRequest = async (option: any) => {
+    const { file } = option;
+    await props
+      .api(option)
+      .then((res) => {
+        file.url = res.data.url;
+        file.status = 'done';
+        fileList.value.pop();
+        fileList.value.push(file);
+      })
+      .catch(() => {
+        fileList.value.pop();
+      });
+  };
 
   function getBase64(file: File) {
     return new Promise((resolve, reject) => {
