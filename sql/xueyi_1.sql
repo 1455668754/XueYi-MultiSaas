ad15b6c63b7cce1d742bdca8558601cec519e3a6
雪忆天堂 <1455668754@qq.com>
2023-08-26 10:42:16 +0800

    build: multiSaas V3.0.0-RC

--- sql/xueyi_1.sql
+++ sql/xueyi_1.sql
@@ -1,4 +1,11 @@
-SET NAMES utf8mb4;
+# DROP DATABASE IF EXISTS `xy-cloud`;
+#
+# CREATE DATABASE  `xy-cloud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+#
+# SET NAMES utf8mb4;
+# SET FOREIGN_KEY_CHECKS = 0;
+#
+# USE `xy-cloud`;
 
 -- ----------------------------
 -- 1、租户信息表
