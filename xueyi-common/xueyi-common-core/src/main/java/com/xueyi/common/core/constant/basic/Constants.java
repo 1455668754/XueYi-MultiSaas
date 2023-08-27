b26717f5dd7c5180c1cf82998a2542863244c9cc
雪忆天堂 <1455668754@qq.com>
2023-08-27 12:11:58 +0800

    build: 升级fastjson到最新版2.0.39

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/basic/Constants.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/constant/basic/Constants.java
@@ -43,4 +43,7 @@ public class Constants {
     /** 资源映射路径 前缀 */
     public static final String RESOURCE_PREFIX = "/profile";
 
+    /** 自动识别json对象白名单配置（仅允许解析的包名，范围越小越安全） */
+    public static final String[] JSON_WHITELIST_STR = {"org.springframework", "com.xueyi"};
+
 }
--- xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/configure/FastJson2JsonRedisSerializer.java
+++ xueyi-common/xueyi-common-redis/src/main/java/com/xueyi/common/redis/configure/FastJson2JsonRedisSerializer.java
@@ -3,6 +3,8 @@ package com.xueyi.common.redis.configure;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONReader;
 import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.filter.Filter;
+import com.xueyi.common.core.constant.basic.Constants;
 import com.xueyi.common.core.utils.core.ArrayUtil;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
@@ -18,6 +20,8 @@ import java.nio.charset.StandardCharsets;
 public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T> {
 
     public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    static final Filter AUTO_TYPE_FILTER = JSONReader.autoTypeFilter(Constants.JSON_WHITELIST_STR);
 
     private Class<T> clazz;
 
@@ -40,6 +44,6 @@ public class FastJson2JsonRedisSerializer<T> implements RedisSerializer<T> {
             return null;
         }
         String str = new String(bytes, DEFAULT_CHARSET);
-        return JSON.parseObject(str, clazz, JSONReader.Feature.SupportAutoType);
+        return JSON.parseObject(str, clazz, AUTO_TYPE_FILTER);
     }
 }
