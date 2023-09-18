9f961cbfc0db980489d9964b33a5c8acd702da22
雪忆天堂 <1455668754@qq.com>
2023-09-18 09:32:53 +0800

    fix: type check failed for 'roles'

--- multi-ui/src/store/modules/permission.ts
+++ multi-ui/src/store/modules/permission.ts
@@ -124,7 +124,7 @@ export const usePermissionStore = defineStore({
         const { roles } = meta || {};
         if (!roles) return true;
         // 进行角色权限判断
-        return roleList.some((role) => roles.includes(role));
+        return roleList.some((role) => (roles as string[]).includes(role));
       };
 
       const routeRemoveIgnoreFilter = (route: AppRouteRecordRaw) => {
