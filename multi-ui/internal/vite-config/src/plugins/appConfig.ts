6a7663daeea283865f7b2d1469e3e6cfec939b74
雪忆天堂 <1455668754@qq.com>
2023-10-10 10:39:51 +0800

    chore(Preview)：消除Preview Options的函数定义类型错误

--- multi-ui/internal/vite-config/src/plugins/appConfig.ts
+++ multi-ui/internal/vite-config/src/plugins/appConfig.ts
@@ -7,16 +7,23 @@ import { strToHex } from '../utils/hash';
 const GLOBAL_CONFIG_FILE_NAME = '_app.config.js';
 // This constant sets the output directory for the Vite package
 const OUTPUT_DIR = 'dist';
-const config: any = getEnvConfig().then();
-const APP_NAME = strToHex(config?.VITE_GLOB_APP_TITLE ?? 'Vben Admin');
 
-export function createConfigPluginConfig(shouldGenerateConfig: boolean): PluginOption {
+export async function createConfigPluginConfig(
+  shouldGenerateConfig: boolean,
+): Promise<PluginOption> {
+  const config = await getEnvConfig();
+  // LINK /src/utils/env.ts -> getVariableName
+  const APP_NAME = strToHex(config?.VITE_GLOB_APP_TITLE ?? '__APP');
   // https://github.com/kirklin/unplugin-config
   return GenerateConfig({
-    disabledConfig: !shouldGenerateConfig,
-    globConfigFileName: GLOBAL_CONFIG_FILE_NAME,
-    outputDir: OUTPUT_DIR,
     appName: APP_NAME,
-    envConfigPrefix: 'VITE_GLOB_',
+    envVariables: {
+      prefix: 'VITE_GLOB_',
+    },
+    configFile: {
+      generate: shouldGenerateConfig,
+      fileName: GLOBAL_CONFIG_FILE_NAME,
+      outputDir: OUTPUT_DIR,
+    },
   });
 }
