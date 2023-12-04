0a3eb06c0d48dfa624c4fda9cf35a84e9ded1f5a
雪忆天堂 <1455668754@qq.com>
2023-12-05 00:54:47 +0800

    feat: 优化LocalDateTimeUtil工具类

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/core/LocalDateTimeUtil.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/core/LocalDateTimeUtil.java
@@ -9,51 +9,60 @@ import java.time.LocalDate;
  */
 public class LocalDateTimeUtil extends cn.hutool.core.date.LocalDateTimeUtil {
 
-    public static LocalDate parseDate(String text) {
-        if (StrUtil.isBlank(text)) {
+    public static LocalDate parseDate(String time) {
+        if (StrUtil.isBlank(time)) {
             return null;
         }
         LocalDate date = null;
         try {
-            date = cn.hutool.core.date.LocalDateTimeUtil.parseDate(text);
+            date = cn.hutool.core.date.LocalDateTimeUtil.parseDate(time);
         } catch (Exception e) {
             try {
                 String replacement = "$1";
                 String regex = ".*(\\d{4}-\\d{2}-\\d{2}).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("1");
-                    date = parseDate(text, "yyyy-MM-dd");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy-MM-dd");
+                }
+                if (ObjectUtil.isNull(date)) {
+                    return date;
                 }
                 regex = ".*(\\d{4}/\\d{2}/\\d{2}).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("3");
-                    date = parseDate(text, "yyyy/MM/dd");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy/MM/dd");
+                }
+                if (ObjectUtil.isNull(date)) {
+                    return date;
                 }
                 regex = ".*(\\d{4}年\\d{2}月\\d{2}日).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("5");
-                    date = parseDate(text, "yyyy年MM月dd日");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy年MM月dd日");
+                }
+                if (ObjectUtil.isNull(date)) {
+                    return date;
                 }
                 regex = ".*(\\d{4}年\\d月\\d{2}日).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("6");
-                    date = parseDate(text, "yyyy年M月dd日");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy年M月dd日");
+                }
+                if (ObjectUtil.isNull(date)) {
+                    return date;
                 }
                 regex = ".*(\\d{4}年\\d{2}月\\d日).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("7");
-                    date = parseDate(text, "yyyy年MM月d日");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy年MM月d日");
+                }
+                if (ObjectUtil.isNull(date)) {
+                    return date;
                 }
                 regex = ".*(\\d{4}年\\d月\\d日).*";
-                if (StrUtil.matches(text, regex)) {
-                    text = StrUtil.replace(text, regex, replacement);
-                    System.out.println("8");
-                    date = parseDate(text, "yyyy年M月d日");
+                if (time.matches(regex)) {
+                    time = time.replaceAll(regex, replacement);
+                    date = LocalDateTimeUtil.parseDate(time, "yyyy年M月d日");
                 }
             } catch (Exception ignored) {
             }
