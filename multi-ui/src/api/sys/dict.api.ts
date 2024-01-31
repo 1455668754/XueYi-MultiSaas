cd34e3fcd1c77d6025da6ad181c7d4972a3938ed
雪忆天堂 <1455668754@qq.com>
2024-01-31 10:23:28 +0800

    fix: 修正参数Api返回类型定义

--- multi-ui/src/api/sys/dict.api.ts
+++ multi-ui/src/api/sys/dict.api.ts
@@ -1,23 +1,25 @@
 import { defHttp } from '@/utils/http/axios';
 import { DictLM } from '@/model/sys';
 
+const basicApi = '/system/admin';
+
 enum Api {
-  CONFIG = '/system/admin/config/config',
-  CONFIG_LIST = '/system/admin/config/configs',
-  DICT = '/system/admin/dict/type/type/',
-  DICT_LIST = '/system/admin/dict/type/types/',
+  CONFIG = basicApi + '/config/config',
+  CONFIG_LIST = basicApi + '/config/configs',
+  DICT = basicApi + '/dict/type/type/',
+  DICT_LIST = basicApi + '/dict/type/types/',
 }
 
 /** 参数查询 */
 export const dicConfig = (code: string) =>
-  defHttp.get<string>({
+  defHttp.get<Map<string, string>>({
     url: Api.CONFIG,
     params: { code: code },
   });
 
 /** 参数批量查询 */
 export const dicConfigList = (codeList: string[]) =>
-  defHttp.get<string>({
+  defHttp.get<Map<string, string>>({
     url: Api.CONFIG_LIST,
     params: { codeList: codeList.toString() },
   });
