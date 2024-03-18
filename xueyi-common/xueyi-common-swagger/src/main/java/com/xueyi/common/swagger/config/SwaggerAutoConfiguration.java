fa40bcb91881cb6a3f61b6f635633bfaaf24d4e9
雪忆天堂 <1455668754@qq.com>
2024-03-18 15:38:43 +0800

    fix: 代码格式化

--- xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerAutoConfiguration.java
+++ xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerAutoConfiguration.java
@@ -15,9 +15,8 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * Swagger文档配置
+ *
  * @author Vondser
- * @version 3.5.0
- * @date 2024/02/29 12:12
  */
 @AutoConfiguration
 @EnableConfigurationProperties(SwaggerProperties.class)
@@ -25,52 +24,52 @@ import org.springframework.context.annotation.Bean;
 public class SwaggerAutoConfiguration {
 
     @Bean
-	public OpenAPI springOpenAPI(SwaggerProperties swaggerProperties) {
-		return new OpenAPI()
-				.info(apiInfo(swaggerProperties))
-				.components(components());
-	}
+    public OpenAPI springOpenAPI(SwaggerProperties swaggerProperties) {
+        return new OpenAPI()
+                .info(apiInfo(swaggerProperties))
+                .components(components());
+    }
 
-	private Info apiInfo(SwaggerProperties swaggerProperties) {
-		return new Info()
-				.title(swaggerProperties.getInfo().getTitle())
-				.description(swaggerProperties.getInfo().getDescription())
-				.license(license(swaggerProperties))
-				.contact(contact(swaggerProperties))
-				.termsOfService(swaggerProperties.getInfo().getTermsOfService())
-				.version(swaggerProperties.getInfo().getVersion());
-	}
+    private Info apiInfo(SwaggerProperties swaggerProperties) {
+        return new Info()
+                .title(swaggerProperties.getInfo().getTitle())
+                .description(swaggerProperties.getInfo().getDescription())
+                .license(license(swaggerProperties))
+                .contact(contact(swaggerProperties))
+                .termsOfService(swaggerProperties.getInfo().getTermsOfService())
+                .version(swaggerProperties.getInfo().getVersion());
+    }
 
-	private License license(SwaggerProperties swaggerProperties) {
-		return new License()
-				.name(swaggerProperties.getInfo().getLicense().getName())
-				.url(swaggerProperties.getInfo().getLicense().getUrl());
-	}
+    private License license(SwaggerProperties swaggerProperties) {
+        return new License()
+                .name(swaggerProperties.getInfo().getLicense().getName())
+                .url(swaggerProperties.getInfo().getLicense().getUrl());
+    }
 
-	private Contact contact(SwaggerProperties swaggerProperties) {
-		return new Contact()
-				.name(swaggerProperties.getInfo().getContact().getName())
-				.url(swaggerProperties.getInfo().getContact().getName())
-				.email(swaggerProperties.getInfo().getContact().getEmail());
-	}
+    private Contact contact(SwaggerProperties swaggerProperties) {
+        return new Contact()
+                .name(swaggerProperties.getInfo().getContact().getName())
+                .url(swaggerProperties.getInfo().getContact().getName())
+                .email(swaggerProperties.getInfo().getContact().getEmail());
+    }
 
-	/**
-	 * 默认的全局鉴权策略
-	 */
-	private Components components() {
-		return new Components()
-				.addSecuritySchemes("token",
-						new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name("Authorization").bearerFormat("JWT"))
-				;
-	}
+    /**
+     * 默认的全局鉴权策略
+     */
+    private Components components() {
+        return new Components()
+                .addSecuritySchemes("token", new SecurityScheme().type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+                        .bearerFormat("JWT"));
+    }
 
-	@Bean
-	public GroupedOpenApi usersGroup() {
-		return GroupedOpenApi.builder().group("all")
-				.addOperationCustomizer((operation, handlerMethod) -> {
-					operation.addSecurityItem(new SecurityRequirement().addList("token"));
-					return operation;
-				})
-				.build();
-	}
+    @Bean
+    public GroupedOpenApi usersGroup() {
+        return GroupedOpenApi.builder().group("all")
+                .addOperationCustomizer((operation, handlerMethod) -> {
+                    operation.addSecurityItem(new SecurityRequirement().addList("token"));
+                    return operation;
+                }).build();
+    }
 }
--- xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerProperties.java
+++ xueyi-common/xueyi-common-swagger/src/main/java/com/xueyi/common/swagger/config/SwaggerProperties.java
@@ -12,81 +12,57 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "springdoc")
 public class SwaggerProperties {
 
-    /**
-     * 文档基本信息
-     */
+    /** 文档基本信息 */
     private InfoProperties info = new InfoProperties();
 
     /**
-     * <p>
      * 文档的基础属性信息
-     * </p>
-     *
      * 为了 springboot 自动生产配置提示信息，所以这里复制一个类出来
      */
     @Data
     public static class InfoProperties {
 
-        /**
-         * 标题
-         */
-        private String title = null;
+        /** 标题 */
+        private String title;
 
-        /**
-         * 描述
-         */
-        private String description = null;
+        /** 描述 */
+        private String description;
 
-        /**
-         * 联系人信息
-         */
-        private Contact contact = null;
+        /** 联系人信息 */
+        private Contact contact;
 
-        /**
-         * 许可证
-         */
-        private License license = null;
+        /** 许可证 */
+        private License license;
 
-        /**
-         * 版本
-         */
-        private String version = null;
+        /** 版本 */
+        private String version;
 
-        /**
-         * 服务条款URL
-         **/
+        /** 服务条款URL **/
         private String termsOfService = "";
 
     }
 
     @Data
-    public static class Contact
-    {
-        /**
-         * 联系人
-         **/
+    public static class Contact {
+
+        /** 联系人 **/
         private String name = "";
-        /**
-         * 联系人url
-         **/
+
+        /** 联系人url **/
         private String url = "";
-        /**
-         * 联系人email
-         **/
+
+        /** 联系人email **/
         private String email = "";
 
     }
 
     @Data
-    public static class License
-    {
-        /**
-         * 联系人
-         **/
+    public static class License {
+
+        /** 联系人 **/
         private String name = "";
-        /**
-         * 联系人url
-         **/
+
+        /** 联系人url **/
         private String url = "";
 
     }
